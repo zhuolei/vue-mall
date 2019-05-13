f734b24ae24bbb1a68a276cdf7554cb6388c120e
zhuolei <dongzhuolei888@gmail.com>
2019-05-12 21:59:42 -0400

    prepare for deployment

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const app = require("./server/app");
+const app = require("./app");
 // const debug = require("debug")("node-angular");
 const http = require("http");
 const debug = require("debug")
